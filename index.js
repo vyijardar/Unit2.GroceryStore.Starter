d76cd9091882b5948d3b9c7baeb78877d81ddcd5
feature-A <vyijardar@YuVi>
2024-10-04 00:44:32 -0500

    Updated with code

--- index.js
+++ index.js
@@ -13,8 +13,21 @@
  * Prints out the name of each item in the given array.
  * @param {Item[]} items - array of items
  */
-function logNames(items) {
-  // TODO: use `forEach`
+const items = [
+  { id: 1, name: "apple", price: 1.75, category: "fruit", quantity: 100 },
+  { id: 2, name: "banana", price: 0.25, category: "fruit", quantity: 137 },
+  { id: 3, name: "orange", price: 1.0, category: "fruit", quantity: 10 },
+  { id: 4, name: "broccoli", price: 3.0, category: "vegetable", quantity: 67 },
+  { id: 6, name: "milk", price: 5.75, category: "dairy", quantity: 90 },
+  { id: 7, name: "cheddar", price: 4.0, category: "dairy", quantity: 63 },
+  { id: 8, name: "sourdough", price: 5.5, category: "grains", quantity: 81 },
+];
+const cart = [];
+function logItemNames() {
+  //TODO: use the .forEach() method to log out the name of each item
+  items.forEach((element) => {
+    console.log(element.name);
+  })
 }
 
 /**
@@ -23,6 +36,9 @@ function logNames(items) {
  */
 function getUppercaseNames(items) {
   // TODO: use `map`
+  return items.map((element) => {
+    return element.name.toUpperCase();
+  })
 }
 
 /**
@@ -32,6 +48,21 @@ function getUppercaseNames(items) {
  */
 function getItemById(items, id) {
   // TODO: use `find`
+  return items.find((element) => {
+    return element.id === id
+  })
+}
+
+ /**
+   * @returns {number} the sum of all inventory items
+   */
+  
+ function calculateTotalInventory() {
+  // TODO Use the .reduce() method to return the total number of items in inventory
+   return items.reduce((acc, item) => {
+    acc = acc + item.inventory
+    return acc
+  },0)
 }
 
 /**
@@ -41,6 +72,10 @@ function getItemById(items, id) {
  */
 function getItemPriceByName(items, name) {
   // TODO: use a loop!
+  let getname = items.find((element) => {
+    return element.name === name
+ })
+ return getname.price;
 }
 
 /**
@@ -50,6 +85,16 @@ function getItemPriceByName(items, name) {
  */
 function getItemsByCategory(items, category) {
   // TODO: use `filter`
+  return items.filter((element) => {
+    return element.category === category;
+})
+}
+function logCartItems() {
+  // TODO: Loop through your cart and use the indexes to log the names of all items in your cart
+  let cartArr = cart.map((element) => {
+   return findItemById(parseInt(element))
+  })
+  cartArr.forEach((element) => console.log(element.name))
 }
 
 /**
@@ -58,6 +103,10 @@ function getItemsByCategory(items, category) {
  */
 function countItems(items) {
   // TODO: use `reduce`
+  return items.reduce((acc, item) => {
+    acc = acc + item.quantity
+    return acc
+  },0)
 }
 
 /**
@@ -66,43 +115,65 @@ function countItems(items) {
  */
 function calculateTotalPrice(items) {
   // TODO: use `reduce`
+  return items.reduce((acc,item) => {
+    acc = acc + item.price
+    return acc
+  },0) 
 }
+
+  
+function logCartItems() {
+  // TODO: Loop through your cart and use the indexes to log the names of all items in your cart
+  let cartArr = cart.map((element) => {
+   return findItemById(parseInt(element))
+  })
+  cartArr.forEach((element) => console.log(element.name))
+}
+
+
 
 // --------------------- DO NOT CHANGE THE CODE BELOW ------------------------ //
 
 /** @type {Item[]} */
-const INVENTORY = [
-  { id: 1, name: "apple", price: 1.75, category: "fruit", quantity: 100 },
-  { id: 2, name: "banana", price: 0.25, category: "fruit", quantity: 137 },
-  { id: 3, name: "orange", price: 1.0, category: "fruit", quantity: 10 },
-  { id: 4, name: "broccoli", price: 3.0, category: "vegetable", quantity: 67 },
-  { id: 6, name: "milk", price: 5.75, category: "dairy", quantity: 90 },
-  { id: 7, name: "cheddar", price: 4.0, category: "dairy", quantity: 63 },
-  { id: 8, name: "sourdough", price: 5.5, category: "grains", quantity: 81 },
-];
+// const INVENTORY = [
+//   { id: 1, name: "apple", price: 1.75, category: "fruit", quantity: 100 },
+//   { id: 2, name: "banana", price: 0.25, category: "fruit", quantity: 137 },
+//   { id: 3, name: "orange", price: 1.0, category: "fruit", quantity: 10 },
+//   { id: 4, name: "broccoli", price: 3.0, category: "vegetable", quantity: 67 },
+//   { id: 6, name: "milk", price: 5.75, category: "dairy", quantity: 90 },
+//   { id: 7, name: "cheddar", price: 4.0, category: "dairy", quantity: 63 },
+//   { id: 8, name: "sourdough", price: 5.5, category: "grains", quantity: 81 },
+// ];
 
+
+  
+  
 console.log("Welcome! We carry the following items:");
-logNames(INVENTORY);
+logItemNames(items);
 
 console.log("Here are the names again in all uppercase:");
-console.log(getUppercaseNames(INVENTORY));
+console.log(getUppercaseNames(items));
 
-console.log(`In total, we have ${countItems(INVENTORY)} items in stock.`);
+console.log(`In total, we have ${countItems(items)} items in stock.`);
 
-const totalCost = calculateTotalPrice(INVENTORY);
+const totalCost = calculateTotalPrice(items);
 console.log(
   `It would cost $${totalCost?.toFixed(2)} to purchase everything in stock.`
 );
 
+
 const itemId = prompt("Enter the ID of an item:", "1");
-console.log(`The item with id #${itemId} is:`);
-console.log(getItemById(INVENTORY, +itemId));
+console.log(`The item with id #${itemId} is: `);
+console.log(getItemById(items, +itemId));
 
 const itemName = prompt("Enter the name of an item:", "apple");
 console.log(
-  `The price of ${itemName} is ${getItemPriceByName(INVENTORY, itemName)}.`
+  `The price of ${itemName} is ${getItemPriceByName(items, itemName)}.`
 );
 
 const category = prompt("Enter a category you would like to see:", "fruit");
-console.log(`The items in the ${category} category are:`);
-console.log(getItemsByCategory(INVENTORY, category));
+console.log(`The items in the ${category} category are: `);
+console.log(getItemsByCategory(items, category));
+
+console.log(
+  `The total price of the items in your cart is: `,calculateTotalPrice(items));
